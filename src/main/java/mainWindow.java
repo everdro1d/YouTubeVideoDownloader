fbb7221514832d049bef71a89bd0649917144fb0
Greg R <68130479+Everdro1d@users.noreply.github.com>
2023-11-10 20:36:44 -0700

    did a whole load of stuff. too tired to think :cries:

--- src/main/java/mainWindow.java
+++ src/main/java/mainWindow.java
@@ -4,22 +4,25 @@ import javax.swing.*;
 import java.awt.*;
 
 public class mainWindow extends JFrame {
-
     protected final JFrame frame;
-    protected JPanel mainPanel;
-    protected JPanel northPanel;
-        protected JPanel northPanelRow1;
-    protected JPanel advancedSettingsPanel;
-    protected JPanel centerVerticalPanel;
-        protected JPanel centerVerticalPanelRow1;
-            protected JButton buttonDownload;
-            protected JCheckBox checkBoxAdvanced;
-        protected JPanel centerVerticalPanelRow2;
-    protected JPanel southPanel;
-    protected JLabel labelTitle;
-    protected JLabel labelURL;
-    protected JTextField textField_URL;
-    protected JComboBox<String> comboBoxVideoAudio;
+        protected JPanel mainPanel;
+            protected JPanel northPanel;
+                protected JPanel northPanelRow1;
+                    protected JLabel labelTitle;
+                    protected JLabel labelURL;
+                    protected JTextField textField_URL;
+            protected JPanel centerVerticalPanel;
+                protected JPanel centerVerticalPanelRow1;
+                    protected JButton buttonDownload;
+                    protected JCheckBox checkBoxAdvanced;
+                protected JPanel centerVerticalPanelRow2;
+                    protected JPanel advancedSettingsPanel;
+                        protected JComboBox<String> comboBoxVideoAudio;
+                        protected JComboBox comboBoxVideoOptions;
+                        protected JComboBox<String> comboBoxAudioOptions;
+            protected JPanel southPanel;
+
+
 
 
     private final String titleText = "YouTube Video Downloader V2.0";
@@ -30,7 +33,7 @@ public class mainWindow extends JFrame {
     public mainWindow() {
         frame = new JFrame();
         frame.setTitle(titleText);
-        frame.setMinimumSize(new Dimension(610, 210));
+        frame.setMinimumSize(new Dimension(610, 228));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
 
@@ -93,11 +96,19 @@ public class mainWindow extends JFrame {
                         northPanelRow1.add(textField_URL);
 
                         //add key listener to the text field
-                           textField_URL.addKeyListener(new java.awt.event.KeyAdapter() {
-                                public void keyReleased(java.awt.event.KeyEvent e) {
-                                    mainWorker.rawURL = textField_URL.getText();
+                        textField_URL.addKeyListener(new java.awt.event.KeyAdapter() {
+                            public void keyReleased(java.awt.event.KeyEvent e) {
+                                mainWorker.rawURL = textField_URL.getText();
+                                if (mainWorker.validURL(mainWorker.rawURL)) {
+                                    System.out.println("URL is valid");
+                                    checkBoxAdvanced.setEnabled(true);
+                                } else {
+                                    System.out.println("URL is invalid");
+                                    checkBoxAdvanced.setSelected(false);
+                                    checkBoxAdvanced.setEnabled(false);
                                 }
-                            });
+                            }
+                        });
 
                     northPanelRow1.add(Box.createRigidArea(new Dimension(10, 0)));
 
@@ -127,34 +138,82 @@ public class mainWindow extends JFrame {
             centerVerticalPanel = new JPanel();
             centerVerticalPanel.setLayout(new BoxLayout(centerVerticalPanel, BoxLayout.Y_AXIS));
                 mainPanel.add(centerVerticalPanel, BorderLayout.CENTER);
+
+            //dependent on the layout of the centerVerticalPanel
             {
                 //create a new JPanel boxlayout in the first row of the centerVerticalPanel
                 centerVerticalPanelRow1 = new JPanel();
                 centerVerticalPanelRow1.setLayout(new BoxLayout(centerVerticalPanelRow1, BoxLayout.LINE_AXIS));
                     centerVerticalPanel.add(centerVerticalPanelRow1);
+
+                //dependent on the layout of the centerVerticalPanelRow1
                 {
                     //add a new checkbox in the center-right of the centerVerticalPanelRow1
                     checkBoxAdvanced = new JCheckBox("Advanced Settings");
                     checkBoxAdvanced.setFont(new Font("Tahoma", Font.PLAIN, 18));
                     checkBoxAdvanced.setAlignmentX(Component.CENTER_ALIGNMENT);
+                    checkBoxAdvanced.setEnabled(false);
                         centerVerticalPanelRow1.add(checkBoxAdvanced);
 
                         checkBoxAdvanced.addActionListener((e) -> {
+                            if (checkBoxAdvanced.isSelected()) {
+                                mainWorker.readVideoOptionsFromYT();
+                                System.out.println("Advanced Settings enabled");
+                            } else {
+                                System.out.println("Advanced Settings disabled");
+                            }
                             advancedSettingsPanel.setVisible(checkBoxAdvanced.isSelected());
                             frame.pack();
                         });
                 }
 
+                centerVerticalPanel.add(Box.createRigidArea(new Dimension(0, 20)));
+
                 //create a new JPanel boxlayout in the second row of the centerVerticalPanel
                 centerVerticalPanelRow2 = new JPanel();
                 centerVerticalPanelRow2.setLayout(new BoxLayout(centerVerticalPanelRow2, BoxLayout.LINE_AXIS));
+                centerVerticalPanelRow2.setSize(new Dimension(600, 200));
                     centerVerticalPanel.add(centerVerticalPanelRow2);
+
+                //dependent on the layout of the centerVerticalPanelRow2
                 {
                     //add a new JPanel in the center of the centerVerticalPanelRow2
                     advancedSettingsPanel = new JPanel();
-                    advancedSettingsPanel.setLayout(new BoxLayout(advancedSettingsPanel, BoxLayout.Y_AXIS));
+                    advancedSettingsPanel.setLayout(new BoxLayout(advancedSettingsPanel, BoxLayout.LINE_AXIS));
+                    advancedSettingsPanel.setVisible(false);
                         centerVerticalPanelRow2.add(advancedSettingsPanel);
+
+                    //dependent on the layout of the advancedSettingsPanel
+                    //TODO: add advanced settings
+                    {
+                        //add two combo boxes in the center of the advancedSettingsPanel
+                        comboBoxVideoOptions = new JComboBox<String>(mainWorker.arrayVideoFormats);
+                        comboBoxVideoOptions.setFont(new Font("Tahoma", Font.PLAIN, 18));
+                        comboBoxVideoOptions.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                        comboBoxVideoOptions.setAlignmentX(Component.CENTER_ALIGNMENT);
+                        comboBoxVideoOptions.setMaximumSize(new Dimension(100, 30));
+                            advancedSettingsPanel.add(comboBoxVideoOptions);
+
+                            comboBoxVideoOptions.addActionListener((e) -> {
+                                comboBoxVideoOptions.getItemAt(comboBoxVideoOptions.getSelectedIndex());
+                            });
+
+                        advancedSettingsPanel.add(Box.createRigidArea(new Dimension(20, 0)));
+
+                        comboBoxAudioOptions = new JComboBox<String>(mainWorker.arrayAudioFormats);
+                        comboBoxAudioOptions.setFont(new Font("Tahoma", Font.PLAIN, 18));
+                        comboBoxAudioOptions.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                        comboBoxAudioOptions.setAlignmentX(Component.CENTER_ALIGNMENT);
+                        comboBoxAudioOptions.setMaximumSize(new Dimension(100, 30));
+                            advancedSettingsPanel.add(comboBoxAudioOptions);
+
+                            comboBoxVideoOptions.addActionListener((e) -> {
+                                comboBoxAudioOptions.getItemAt(comboBoxAudioOptions.getSelectedIndex());
+                            });
+
+                    }
                 }
+                centerVerticalPanel.add(Box.createRigidArea(new Dimension(0, 20)));
             }
 
 
--- src/main/java/mainWorker.java
+++ src/main/java/mainWorker.java
@@ -19,11 +19,20 @@ package main.java;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class mainWorker {
 
     public static String rawURL; // raw URL String from the text field
     public static int videoAudio; // 0 = video, 1 = audio
+    private static final String downloadBinary = "libs/yt-dlp.exe "; // the path to the binary to run
+    protected static String filePath = ""; // the path to download the video to
+
+    protected static String[] arrayVideoFormats = new String[]
+            {"mp4", "mkv", "webm", "avi", "mov"};
+    protected static String[] arrayAudioFormats = new String[]
+            {"m4a", "mp3", "aac", "flac", "ogg", "opus", "vorbis", "aiff", "opus"};
+
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -37,10 +46,158 @@ public class mainWorker {
                 ex.printStackTrace(System.out);
             }
         });
+        checkUpdate();
+    }
+
+    public static void checkUpdate() {
+        try {
+            new Thread(()->{
+                Process p;
+                try {
+                    p = Runtime.getRuntime().exec(downloadBinary + "-U");
+                    new Thread(new SyncPipe(p.getErrorStream(), System.err)).start();
+                    new Thread(new SyncPipe(p.getInputStream(), System.out)).start();
+                    p.waitFor();
+                    System.out.println(p.exitValue());
+                } catch (Exception e) {
+                    e.printStackTrace(System.out);
+                }
+            }).start();
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+    public static void readVideoOptionsFromYT() {
+        //TODO
+
+        // get the video options from the URL
+        getVideoOptions();
+
+    }
+    private static void getVideoOptions() {
+        String cmd = downloadBinary + "--list-formats " + rawURL;
+
+        try {
+            new Thread(()->{
+                Process p;
+                try {
+                    p = Runtime.getRuntime().exec(cmd);
+                    new Thread(new SyncPipe(p.getErrorStream(), System.err)).start();
+                    new Thread(new SyncPipe(p.getInputStream(), System.out)).start();
+                    p.waitFor();
+                    //System.out.println(p.exitValue());
+                } catch (Exception e) {
+                    e.printStackTrace(System.out);
+                }
+            }).start();
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+    protected static boolean validURL(String url) {
+        // check if the URL is valid
+        if ( !(url.contains("youtube.com") || url.contains("youtu.be")) ) {
+            return false;
+        }
+        try {
+            (new java.net.URL(url)).openStream().close();
+            return true;
+        } catch (Exception ignored) { }
+        return false;
+    }
+
+    private static boolean checkURLDialog() {
+        if ((rawURL == null) || !validURL(rawURL)) {
+            JOptionPane.showMessageDialog(null, "Please enter a valid YouTube URL.", "Error", JOptionPane.ERROR_MESSAGE);
+            return true;
+        }
+        return false;
     }
 
     public static void downloadButtonClicked() {
+        // start download
+        if ( checkURLDialog() ) { return; }
+
+        filePath = getFilePath();
+        String cmd = getCommand();
+        download(cmd);
+
         System.out.println("Download button clicked");
-        System.out.println("URL: " + rawURL);
+        System.out.println("command to run: " + cmd);
     }
+
+    public static String getCommand() {
+        // the options to pass to the binary
+        String advancedOptions = getAdvancedOptions();
+
+        return downloadBinary + advancedOptions + "-o \"" + filePath + "%(title)s.%(ext)s\" " + rawURL;
+    }
+
+    public static String getAdvancedOptions() {
+        String output = "";
+        ArrayList<String> arrayListAdvancedOptions = new ArrayList<String>();
+
+        //TODO
+        String defaultVideoOptions = "--restrict-filenames -S res:1080,ext:" + arrayVideoFormats[0] + ":" + arrayAudioFormats[0];
+        String defaultAudioOptions = "--restrict-filenames -x --audio-format " + arrayAudioFormats[0] + " --audio-quality 0";
+
+        if (videoAudio == 0) {
+            arrayListAdvancedOptions.add(defaultVideoOptions);
+        } else if (videoAudio == 1) {
+            arrayListAdvancedOptions.add(defaultAudioOptions);
+        }
+        //TODO
+        // add the options to the cmd variable
+
+
+
+        for (int i = 0; i < arrayListAdvancedOptions.size() ; i++) {
+            // add the options to the cmd variable
+            output += arrayListAdvancedOptions.get(i) + " ";
+        }
+
+        return output;
+    }
+
+    public static void download(String cmd) {
+        // start download
+        try {
+            new Thread(()->{
+                Process p;
+                try {
+                    p = Runtime.getRuntime().exec(cmd);
+                    //make a working dialog
+                    //show the dialog
+                    new Thread(new SyncPipe(p.getErrorStream(), System.err)).start();
+                    new Thread(new SyncPipe(p.getInputStream(), System.out)).start();
+                    p.waitFor();
+                    //dispose of the dialog
+                    JOptionPane.showConfirmDialog(null, "Done", "Finished!", JOptionPane.DEFAULT_OPTION);
+                } catch (Exception e) {
+                    e.printStackTrace(System.out);
+                }
+            }).start();
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+    public static String getFilePath() {
+        // TODO
+        // 1. get the file path from the file chooser
+        // 2. update the file path variable
+
+
+        String output = "";
+
+        if (output.equals("")) {
+            output = (System.getProperty("user.home") + "\\Downloads\\");
+        }
+        output = "C:\\Users\\everd\\Documents\\IntelliJ\\YoutubeVideoDownloaderV2\\";
+        return output;
+    }
+
+
 }
